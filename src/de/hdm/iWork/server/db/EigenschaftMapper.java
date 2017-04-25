f90d330355e77a848c7f42da391a72f9065a7860
Dunja <dt021@hdm-stuttgart.de>
2017-04-25 11:14:59 +0200

    Eigenschaft und Inhalt bo-Klassen hinzugefügt und die Mapper Klassen dazu.

---
+++ src/de/hdm/iWork/server/db/EigenschaftMapper.java
@@ -0,0 +1,131 @@
+package de.hdm.iWork.server.db;
+
+import java.sql.*;
+
+
+import com.google.cloud.sql.jdbc.Connection;
+
+import de.hdm.iWork.shared.bo.*;
+
+public class EigenschaftMapper {
+	
+	/*
+	 * statische Variable
+	 */
+	
+	private static EigenschaftMapper eigenschaftMapper=null;
+	
+	/**
+	 * Geschützter Konstruktor
+	 */
+	protected EigenschaftMapper(){
+		
+	}
+	
+	/**
+	 * Statische Methode die Singeltone Eigenschaft sicherstellt
+	 * @return
+	 */
+	
+	public static EigenschaftMapper eigenschaftMapper(){
+		if(eigenschaftMapper == null){
+			eigenschaftMapper = new EigenschaftMapper();
+			
+		}
+		
+		return eigenschaftMapper;
+	}
+	
+	public Eigenschaft anlegenEigenschaft(Eigenschaft eigenschaft){
+		Connection con= DBConnection.connection();
+		
+		try{
+			Statement stmt= con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT MAX(id) AS maxid "
+			          + "FROM eigenschaft ");
+			
+			if(rs.next()){
+				
+				/*
+				 * EigenschaftId des Objekts wird gesetzt und dabei um 1 erhöht
+				 */
+				eigenschaft.setEigenschaftId(rs.getInt("maxid")+1);
+				
+				stmt = con.createStatement();
+				
+				/*
+				 * Einfügeoperation
+				 */
+				stmt.executeUpdate("INSERT INTO eigenschaft (eigenschaftId,bezeichnung) " + "VALUES ("
+						+ eigenschaft.getEigenschaftId() + "," + eigenschaft.getBezeichnung() + ")");
+				
+			}
+		}
+		
+		catch (SQLException e2){
+			e2.printStackTrace();
+		}
+		
+		return eigenschaft;
+	}
+	
+	public Eigenschaft updateEigenschaft(Eigenschaft eigenschaft){
+		Connection con= DBConnection.connection();
+		
+		try{
+			Statement stmt = con.createStatement();
+			
+			stmt.executeUpdate("UPDATE eigenschaft " + "SET bezeichnung\"" + eigenschaft.getBezeichnung()
+			+ "\" " + "WHERE eigenschaftId=" + eigenschaft.getEigenschaftId());
+			
+		}
+		
+		catch(SQLException e2){
+			e2.printStackTrace();
+		}
+		
+		return eigenschaft;
+	}
+	
+	public void loeschenEigenschaft(Eigenschaft eigenschaft){
+		Connection con = DBConnection.connection();
+		
+		try{
+			
+			Statement stmt = con.createStatement();
+			
+			stmt.executeUpdate("DELETE FROM eigenschaft " + "WHERE eigenschaftId=" + eigenschaft.getEigenschaftId());
+				
+		}
+		
+		catch(SQLException e2){
+			e2.printStackTrace();
+		}
+	}
+	
+	public Eigenschaft getEigenschaftbyId(int eigenschaftId){
+		Connection con = DBConnection.connection();
+		
+		try{
+			Statement stmt = con.createStatement();
+			
+			ResultSet rs = stmt.executeUpdate("SELECT eigenschaftId, bezeichnung FROM eigenschaft "
+					+ "WHERE eigenschaftId=" + eigenschaftId);
+			
+			if(rs.next()){
+				Eigenschaft eigenschaft = new Eigenschaft();
+				eigenschaft.setEigenschaftId(rs.getInt("eigenschaftId"));
+				eigenschaft.setBezeichnung(rs.getString("bezeichnung"));
+				return eigenschaft;
+				
+			}
+		}
+		
+		catch(SQLException e2){
+			e2.printStackTrace();
+			return null;
+		}
+		
+		return null;
+	}
+}
