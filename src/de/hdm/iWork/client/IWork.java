1760c66f26a84cc8c45e11a60914dd41c040121b
Dunja <dt021@hdm-stuttgart.de>
2017-05-07 17:00:20 +0200

    Klasse IWork angelegt mit EntryPoint, IWork.gwt.xml angelegt und EntryPoint zugewiesen

---
+++ src/de/hdm/iWork/client/IWork.java
@@ -0,0 +1,25 @@
+package de.hdm.iWork.client;
+
+import com.google.gwt.core.client.*;
+import com.google.gwt.user.client.ui.*;
+
+
+/**
+ * Entry-Point Klasse
+ * @author Dunja
+ *
+ */
+
+public class IWork implements EntryPoint {
+	
+	
+
+	@Override
+	public void onModuleLoad() {
+		
+		VerticalPanel navPanel = new VerticalPanel();
+		RootPanel.get("Navigator").add(navPanel);
+		
+	}
+
+}
