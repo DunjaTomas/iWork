830dd5b0ed62f6e4e60861f9f427b8b10dae54e2
Dunja <dt021@hdm-stuttgart.de>
2017-05-07 20:34:50 +0200

    Button zu Menubar hinzugefügt.

--- src/de/hdm/iWork/client/IWork.java
+++ src/de/hdm/iWork/client/IWork.java
@@ -17,8 +17,9 @@ public class IWork implements EntryPoint {
 	@Override
 	public void onModuleLoad() {
 		
-		VerticalPanel navPanel = new VerticalPanel();
-		RootPanel.get("Navigator").add(navPanel);
+			
+			RootPanel.get("Navigator").add(new Navigator ());
+		
 		
 	}
 
---
+++ src/de/hdm/iWork/client/Navigator.java
@@ -0,0 +1,75 @@
+package de.hdm.iWork.client;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class Navigator  extends HorizontalPanel {
+	
+	VerticalPanel navPanel = new VerticalPanel();
+	
+	
+	public Navigator(){
+		
+		run();
+		
+		
+	}
+	
+public void run(){
+	
+	this.add(navPanel);
+	
+	Button profilAnzeigenButton = new  Button("Mein Profil");
+	profilAnzeigenButton.setStylePrimaryName("iWork-menubutton");
+	
+	profilAnzeigenButton.addClickHandler(new ClickHandler (){
+
+		@Override
+		public void onClick(ClickEvent event) {
+			// TODO Auto-generated method stub
+			
+		}
+			
+	});
+	
+	Button stellenbeschreibungenAnzeigenButton = new Button("Jobs");
+	stellenbeschreibungenAnzeigenButton.setStylePrimaryName("iWork-menubutton");
+	
+	stellenbeschreibungenAnzeigenButton.addClickHandler(new ClickHandler(){
+
+		@Override
+		public void onClick(ClickEvent event) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	});
+	
+	Button bewerbungenAnzeigenButton = new Button("Meine Bewerbungen");
+	bewerbungenAnzeigenButton.setStylePrimaryName("iWork-menubutton");
+	
+	bewerbungenAnzeigenButton.addClickHandler(new ClickHandler(){
+
+		@Override
+		public void onClick(ClickEvent event) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	});
+	
+	navPanel.add(profilAnzeigenButton);
+	navPanel.add(stellenbeschreibungenAnzeigenButton);
+	navPanel.add(bewerbungenAnzeigenButton);
+	
+	
+	
+}
+
+
+
+}
