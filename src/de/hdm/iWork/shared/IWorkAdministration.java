34084e5f55c906010dec677d5872c34541e18a9d
CarolinMarian <carolin.marian95@googlemail.com>
2017-05-09 14:30:42 +0200

    Servlets für Editor eingerichtet

--- src/de/hdm/iWork/shared/IWorkAdministration.java
+++ src/de/hdm/iWork/shared/IWorkAdministration.java
@@ -3,10 +3,13 @@ package de.hdm.iWork.shared;
 import java.util.Vector;
 
 import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 import de.hdm.iWork.shared.bo.*;
 
 
+
+@RemoteServiceRelativePath("iworkadministration")
 public interface IWorkAdministration extends RemoteService {
 
 	public void init() throws IllegalArgumentException;
